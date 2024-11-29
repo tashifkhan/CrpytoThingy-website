1ec54212619e8d87be04baf09afb546be7af1bbc
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2024-11-29 23:04:28 +0530

    merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import CardBanner from "@/components/CardBanner";
 import { AnimatedTestimonialsDemo } from "@/components/Testies";
 import ShibariumComponent from "@/components/Block";
 import Hero from "@/components/Hero";
+import Banneer from "@/components/Banneer";
+import ShibaSwapPage from "@/components/ShibaSwap";
+import ShiboshisBanner from "@/components/Shiboshis";
 
 export default function Home() {
 	return (
@@ -10,18 +13,16 @@ export default function Home() {
 			<Hero />
 			<ShibariumComponent />
 			<AnimatedTestimonialsDemo />
-<div className="w-[80%] mx-auto mt-12">
+			<div className="w-[80%] mx-auto mt-12">
 				<Banneer />
 			</div>
 
-			<div className="w-[100%] mx-auto mt-12"> 
-                 <ShibaSwapPage/>
-
+			<div className="w-[100%] mx-auto mt-12">
+				<ShibaSwapPage />
 			</div>
-           
-			<div className="w-[100%] mx-auto mt-12"> 
-                 <ShiboshisBanner/>
 
+			<div className="w-[100%] mx-auto mt-12">
+				<ShiboshisBanner />
 			</div>
 		</>
 	);
