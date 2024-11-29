b9fa1d2fa2a562321d83799c922c811b5794fd0a
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2024-11-29 22:48:04 +0530

    added block

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import CardBanner from "@/components/CardBanner";
 import { AnimatedTestimonialsDemo } from "@/components/Testies";
+import ShibariumComponent from "@/components/Block";
 
 export default function Home() {
 	return (
@@ -9,6 +10,7 @@ export default function Home() {
 				<h1 className="text-3xl font-bold mt-[12rem]">Shiba Inu Tokens</h1>
 			</div>
 			<CardBanner />
+			<ShibariumComponent />
 			<AnimatedTestimonialsDemo />
 		</>
 	);
