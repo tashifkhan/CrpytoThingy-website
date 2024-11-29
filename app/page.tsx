44e96e20ebb35031c29256d349ff52edafe38c25
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2024-11-29 23:07:42 +0530

    components ordered

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import CardBanner from "@/components/CardBanner";
 import { AnimatedTestimonialsDemo } from "@/components/Testies";
 import ShibariumComponent from "@/components/Block";
 import Hero from "@/components/Hero";
@@ -11,19 +9,17 @@ export default function Home() {
 	return (
 		<>
 			<Hero />
-			<ShibariumComponent />
-			<AnimatedTestimonialsDemo />
 			<div className="w-[80%] mx-auto mt-12">
 				<Banneer />
 			</div>
-
 			<div className="w-[100%] mx-auto mt-12">
 				<ShibaSwapPage />
 			</div>
-
 			<div className="w-[100%] mx-auto mt-12">
 				<ShiboshisBanner />
 			</div>
+			<ShibariumComponent />
+			<AnimatedTestimonialsDemo />
 		</>
 	);
 }
