525792080e86dd0bbc9346c14170204ec2572288
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2024-12-05 22:45:18 +0530

    market fixed

--- app/table/page.tsx
+++ app/table/page.tsx
@@ -1,14 +1,40 @@
+"use client";
 import Table from "@/components/Data";
 import React from "react";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 
 export default function TablePage() {
+	const { ref, inView } = useInView({
+		triggerOnce: true,
+		threshold: 0.1,
+	});
 	return (
-		<div className="min-h-screen bg-gradient-to-b from-slate-900 to-slate-800 p-4 flex items-center justify-center">
-			<div className="max-w-3xl w-full mx-auto p-8">
-				<h1 className="text-4xl md:text-5xl font-bold mb-12 text-slate-200 text-center">
+		<div className="min-h-screen bg-gradient-to-r from-slate-950 to-black p-4 flex items-center justify-center">
+			<div ref={ref} className="max-w-3xl w-full mx-auto p-8">
+				<motion.h1
+					className="text-4xl md:text-5xl font-bold mb-12 text-slate-200 text-center yuji-mai-regular"
+					initial={{ opacity: 0, y: -50 }}
+					animate={inView ? { opacity: 1, y: 0 } : {}}
+					transition={{ duration: 0.5 }}
+				>
 					Market Data
-				</h1>
-				<Table />
+				</motion.h1>
+
+				<motion.div
+					initial={{ opacity: 0, y: 50 }}
+					animate={inView ? { opacity: 1, y: 0 } : {}}
+					transition={{ duration: 0.5, delay: 0.2 }}
+				>
+					<Table />
+				</motion.div>
+
+				<motion.div
+					initial={{ opacity: 0, scale: 0.8 }}
+					animate={inView ? { opacity: 1, scale: 1 } : {}}
+					transition={{ duration: 0.6, delay: 0.4 }}
+					className="max-w-3xl w-full mx-auto p-8"
+				></motion.div>
 			</div>
 		</div>
 	);
